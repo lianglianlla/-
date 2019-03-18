ccae9d738e47d7ff8a12e24e1fda869829b770ca
lianglianlla <523524306@qq.com>
2019-03-18 13:54:43 +0800

    算法设计第一章

---
+++ Ex1.java
@@ -0,0 +1,9 @@
+public class Ex1 {
+    public static void main(String args[]){
+        System.out.println("**********");
+        System.out.println("学号");
+        System.out.println("姓名");
+        System.out.println("**********");
+        //或者：System.out.print("**********\n学号\n姓名\n***********");
+    }
+}
